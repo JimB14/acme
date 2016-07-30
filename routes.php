67edcd4f241df807128cccc3261f061b28e11a6e
JimB14 <jim.burns14@gmail.com>
2016-07-30 15:31:44 -0400

    Revert "Initial commit"
    This reverts commit 6783a07bb58ce6488298f797283321a263432eae.

--- routes.php
+++
@@ -1,9 +0,0 @@
-<?php
-// routes to reach files (a map shows a path to something)
-$router->map('GET', '/', 'Acme\Controllers\PageController@getShowHomePage', 'home');
-
-$router->map('GET', '/register', 'Acme\Controllers\RegisterController@getShowRegisterPage', 'register');
-
-$router->map('POST', '/register', 'Acme\Controllers\RegisterController@postShowRegisterPage', 'register_post');
-
-$router->map('GET', '/login', 'Acme\Controllers\RegisterController@getShowLoginPage', 'login');
