b2719e025dcf558e94341b7d9a62e3d98222ad66
JimB14 <jim.burns14@gmail.com>
2016-08-08 14:31:49 -0400

    Updates

--- migrations/20160731151834_seed_pages_table.php
+++ migrations/20160731151834_seed_pages_table.php
@@ -9,10 +9,36 @@ class SeedPagesTable extends AbstractMigration
   {
 
       $this->execute("
-          INSERT INTO pages (browser_title, page_content)
-          VALUES
-          ('About', 'Acme Tours has been providing national and international tour services since 1985. Our services are available for organizations, groups, families and individuals.')
-      ");
+            INSERT INTO pages
+            (browser_title, page_content, slug)
+            VALUES
+            ('About Acme', '<h1>About Acme</h1><p>All about this company.</p>', 'about-acme')
+        ");
+        $this->execute("
+            INSERT INTO pages
+            (browser_title, page_content, slug)
+            VALUES
+            ('Success', '<h1>Success</h1><p>Welcome to Acme!</p>', 'success')
+        ");
+        $this->execute("
+            INSERT INTO pages
+            (browser_title, page_content, slug)
+            VALUES
+            ('Not Found', '<h1>Page Not Found!</h1><p>Page not found!</p>', 'page-not-found')
+        ");
+        $this->execute("
+            INSERT INTO pages
+            (browser_title, page_content, slug)
+            VALUES
+            ('Account Activated', '<h1>Acount Now Active</h1><p>Your account is now active, and you can log in.</p>', 'account-activated')
+        ");
+        $this->execute("
+            INSERT INTO pages
+            (browser_title, page_content, slug)
+            VALUES
+            ('Saved', '<h1>Testimonial Saved</h1><p>Your testimonial has been saved.</p>', 'testimonial-saved')
+        ");
+
   }
 
   public function down()
--- migrations/20160731153210_seed_testimonials_table.php
+++ migrations/20160731153210_seed_testimonials_table.php
@@ -8,9 +8,14 @@ class SeedTestimonialsTable extends AbstractMigration
   public function up()
   {
       $this->execute("
-          INSERT INTO testimonials (title, testimonial, user_id)
+          INSERT INTO testimonials (title, testimonial, user_id, created_at)
           VALUES
-          ('Best Tour Company around', 'Acme Tours is the best tour company I have traveled with. Not only did they take us to the best places, show us how to avoid the tourist traps, but they educated us in a way that magnified an already great experience.', '1')
+          (
+          'Best Tour Company around',
+          'Acme Tours is the best tour company I have traveled with. Not only did they take us to the best places, show us how to avoid the tourist traps, but they educated us in a way that magnified an already great experience.',
+          '1',
+          '2016-08-04 22:18:36'
+          )
       ");
   }
 
